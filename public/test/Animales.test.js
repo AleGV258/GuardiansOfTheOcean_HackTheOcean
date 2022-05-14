78858e975f3ca806cb744635af428ab31b8451fc
Michell Alejandro GV <ale.gv258@gmail.com>
2022-05-14 13:12:42 -0500

    Agregar Pruebas de Unidad de los Animales

--- public/test/Animales.test.js
+++ public/test/Animales.test.js
@@ -3,18 +3,18 @@ const AnimalesService = require("../app/services/Animales");
 describe("Pruebas de unidad AnimalesService", () => {
     test("1: Acceso a todos los datos de la DB", () => {
         const totalAnimals = AnimalesService.getMultiple();
-        expect(totalAnimals.length).toBe(10);
+        expect(totalAnimals.length).toBe(18);
     });
 
-    test("2: Acceso a los datos del animal 'Trucha Apache' (verificación 'UbicacioPreliminar')", () => {
-        const animal = "Trucha apache";
+    test("2: Acceso a los datos del animal 'Pez Totoaba' (verificación 'UbicacioPreliminar')", () => {
+        const animal = "Pez Totoaba";
         const datosAnimal = AnimalesService.getAnimal(animal);
-        expect(datosAnimal[0].UbicacionPreliminar).toBe('Arizona, Estados Unidos');
+        expect(datosAnimal[0].UbicacionPreliminar).toBe('El Norte del Golfo de California de México.');
     });
 
-    test("3: Acceso a los datos del animal 'Ajolote' (verificación 'Causa de Peligro')", () => {
-        const animal = "Ajolote";
+    test("3: Acceso a los datos del animal 'Ajolote Mexicano' (verificación 'Causa de Peligro')", () => {
+        const animal = "Ajolote Mexicano";
         const datosAnimal = AnimalesService.getAnimal(animal);
-        expect(datosAnimal[0].CausaDePeligro).toBe('Pérdida de hábitat, introducción de peces exóticos, sobreexplotación, contaminación y su consumo como alimento.');
+        expect(datosAnimal[0].CausaDePeligro).toBe('La pérdida de hábitat, su uso como mascotas exóticas, la contaminación, y su consumo como alimento.');
     });
 });

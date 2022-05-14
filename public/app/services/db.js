1a2b49e2f7695a2c3520d48276c01816971c9181
LJosueCM <ljosuecm@gmail.com>
2022-05-14 11:28:38 -0500

    Añadido servicio db para conexión con DB y devolucion de data

---
+++ public/app/services/db.js
@@ -0,0 +1,11 @@
+const sqlite = require('better-sqlite3');
+const path = require('path');
+const db = new sqlite(path.resolve('Animales.db'), {fileMustExist: true});
+
+function query(sql) {
+  return db.prepare(sql).all();
+}
+
+module.exports = {
+  query
+}

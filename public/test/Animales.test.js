7cd0e8aa73dee3b5c53e576a2943ce2b8cd4c1ac
LJosueCM <ljosuecm@gmail.com>
2022-05-14 11:24:51 -0500

    Establecidas unit test para acceso a la DB

---
+++ public/test/Animales.test.js
@@ -0,0 +1,20 @@
+const AnimalesService = require("../app/services/Animales");
+
+describe("Pruebas de unidad AnimalesService", () => {
+    test("1: Acceso a todos los datos de la DB", () => {
+        const totalAnimals = AnimalesService.getMultiple();
+        expect(totalAnimals.length).toBe(10);
+    });
+
+    test("2: Acceso a los datos del animal 'Trucha Apache' (verificación 'UbicacioPreliminar')", () => {
+        const animal = "Trucha Apache";
+        const datosAnimal = AnimalesService.getAnimal(animal);
+        expect(datosAnimal[0].UbicacionPreliminar).toBe('Arizona, Estados Unidos');
+    });
+
+    test("3: Acceso a los datos del animal 'Ajolote' (verificación 'Causa de Peligro')", () => {
+        const animal = "Ajolote";
+        const datosAnimal = AnimalesService.getAnimal(animal);
+        expect(datosAnimal[0].CausaDePeligro).toBe('Pérdida de hábitat, introducción de peces exóticos, sobreexplotación, contaminación y su consumo como alimento.');
+    });
+});
